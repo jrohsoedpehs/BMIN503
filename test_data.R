c70e2c66c5207c7bd990d497d77cace092353841
jrohsoedpehs <42783297+jrohsoedpehs@users.noreply.github.com>
2018-09-28 14:00:04 -0400

    Add files via upload

---
+++ test_data.R
@@ -0,0 +1,7 @@
+x <-
+structure(list(V1 = 1:5, V2 = structure(c(1L, 1L, 1L, 1L, 1L), .Label = "Y", class = "factor")), class = "data.frame", row.names = c(NA, 
+-5L))
+y <-
+1:10
+z <-
+"Some text."
